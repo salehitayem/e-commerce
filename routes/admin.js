708d7970fe69af32504eae00db86362faff28038
Saleh Itayem <salehktaweel@gmail.com>
2023-11-28 23:25:53 +0200

    first commit

---
+++ routes/admin.js
@@ -0,0 +1,19 @@
+const path = require('path');
+
+const express = require('express');
+
+const rootDir = require('../util/path');
+
+const router = express.Router();
+// /admin/add-product => get
+router.get('/add-product', (req, res, next) => {
+    res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+});
+// /admin/add-product => post
+router.post('/add-product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/')
+
+});
+
+module.exports = router;
