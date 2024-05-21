5057a66e8fe55043191f7ba51eb7c97cc90e5e32
salehitayem <salehktaweel@gmail.com>
2024-05-21 12:25:36 +0300

    update #

--- routes/shop.js
+++ routes/shop.js
@@ -2,20 +2,10 @@ const path = require('path');
 
 const express = require('express');
 
-const rootDir = require('../util/path');
-const adminData = require('../controllers/product-list');
+const productsController = require('../controllers/products');
 
 const router = express.Router();
 
-
-router.get('/', (req, res, next) => {
-    const products = adminData.products;
-    res.render('shop', {
-        prods: products,
-        docTitle: 'Shop',
-        path: 'shop'
-        });
-});
-
+router.get('/', productsController.getProducts);
 
 module.exports = router;
